19547f19d1e409fb2362fb6ce5bc68f8373c6944
Sophia5481 <sophiashenyongen@gmail.com>
2022-06-26 13:38:15 -0700

    Added my phone number

--- GetMaxOfArray.java
+++ GetMaxOfArray.java
@@ -11,5 +11,6 @@ public class GetMaxOfArray {
    public static void main(String[] args) {
       int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6}; 
       System.out.println(max(numbers));
+      System.out.println("My phone number is 12345678.");
    }
 }
